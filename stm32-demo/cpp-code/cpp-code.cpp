5a0b026208817c2890a07225f38a75904df71657
Alexey Bulatov <AlexeyABulatov@yandex.ru>
2024-05-01 20:45:58 +0300

    Task text added

--- stm32-demo/cpp-code/cpp-code.cpp
+++ stm32-demo/cpp-code/cpp-code.cpp
@@ -1,5 +1,6 @@
 #include "cpp-code.h"
-#include <vector>
+#include <map>
+#include <string>
 
 #include "stm32f1xx_hal.h"
 
@@ -7,10 +8,23 @@ static const unsigned char cpp_text[] = "Hello, stm32 C++ developer!\r\n";
 
 extern UART_HandleTypeDef huart1;
 
+void function_to_investigate()
+{
+    std::map<std::string, int> m;
+    for (int i = 1; i <= 10; ++i)
+    {
+        m[std::to_string(i)] = i;
+    }
+}
+
+void print_stat()
+{
+    // Some code here should print statistic of _sbrk, malloc, free usage
+    HAL_UART_Transmit(&huart1, cpp_text, sizeof(cpp_text), 100);
+}
+
 void cpp_code_entry_point()
 {
-    // Some example that proof C++ code works
-    std::vector<int> v(10);
-    v.push_back(1234);
-    HAL_UART_Transmit(&huart1, cpp_text, sizeof(cpp_text), 100);
+    function_to_investigate();
+    print_stat();
 }
